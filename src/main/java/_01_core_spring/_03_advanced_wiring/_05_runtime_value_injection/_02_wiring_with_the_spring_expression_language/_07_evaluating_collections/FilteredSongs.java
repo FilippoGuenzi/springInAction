e60629cdc06daeb03ecb153950d987d31df8d2fd
fguenzi <fguenzi@egencia.com>
2016-09-06 09:01:14 +0200

    finished with SpEL

--- src/main/java/_01_core_spring/_03_advanced_wiring/_05_runtime_value_injection/_02_wiring_with_the_spring_expression_language/_07_evaluating_collections/FilteredSongs.java
+++ src/main/java/_01_core_spring/_03_advanced_wiring/_05_runtime_value_injection/_02_wiring_with_the_spring_expression_language/_07_evaluating_collections/FilteredSongs.java
@@ -11,11 +11,44 @@ public class FilteredSongs {
 
     private List<Song> songsOfU2;
     private List<Song> songsOfBobDylan;
+    private Song firstSongOfU2;
+    private Song firstSongOfBobDylan;
+    private Song lastSongOfU2;
+    private Song lastSongOfBobDylan;
+    private List<String> titlesOfU2;
+    private List<String> titlesOfBobDylan;
+    private Song randomSongOfCD;
 
     @Autowired
-    public FilteredSongs(@Value("#{compactDisc.songs.?[author == 'U2']}") List<Song> songsOfU2, @Value("#{compactDisc.songs.?[author == 'Bob Dylan']}") List<Song> songsOfBobDylan) {
+    public FilteredSongs(
+            @Value("#{compactDisc.songs.?[author == 'U2']}")
+            List<Song> songsOfU2,
+            @Value("#{compactDisc.songs.?[author == 'Bob Dylan']}")
+            List<Song> songsOfBobDylan,
+            @Value("#{compactDisc.songs.^[author == 'U2']}")
+            Song firstSongOfU2,
+            @Value("#{compactDisc.songs.^[author == 'Bob Dylan']}")
+            Song firstSongOfBobDylan,
+            @Value("#{compactDisc.songs.$[author == 'U2']}")
+            Song lastSongOfU2,
+            @Value("#{compactDisc.songs.$[author == 'Bob Dylan']}")
+            Song lastSongOfBobDylan,
+            @Value("#{compactDisc.songs.?[author == 'U2'].![title]}")
+            List<String> titlesOfU2,
+            @Value("#{compactDisc.songs.?[author == 'Bob Dylan'].![title]}")
+            List<String> titlesOfBobDylan,
+            @Value("#{compactDisc.songs[T(java.lang.Math).floor(T(java.lang.Math).random() * compactDisc.songs.size())]}")
+            Song randomSongOfCD
+    ) {
         this.songsOfU2 = songsOfU2;
         this.songsOfBobDylan = songsOfBobDylan;
+        this.firstSongOfU2 = firstSongOfU2;
+        this.firstSongOfBobDylan = firstSongOfBobDylan;
+        this.lastSongOfU2 = lastSongOfU2;
+        this.lastSongOfBobDylan = lastSongOfBobDylan;
+        this.titlesOfU2 = titlesOfU2;
+        this.titlesOfBobDylan = titlesOfBobDylan;
+        this.randomSongOfCD = randomSongOfCD;
     }
 
     public List<Song> getSongsOfU2() {
@@ -24,5 +57,33 @@ public class FilteredSongs {
 
     public List<Song> getSongsOfBobDylan() {
         return songsOfBobDylan;
+    }
+
+    public Song getFirstSongOfU2() {
+        return firstSongOfU2;
+    }
+
+    public Song getFirstSongOfBobDylan() {
+        return firstSongOfBobDylan;
+    }
+
+    public Song getLastSongOfU2() {
+        return lastSongOfU2;
+    }
+
+    public Song getLastSongOfBobDylan() {
+        return lastSongOfBobDylan;
+    }
+
+    public List<String> getTitlesOfU2() {
+        return titlesOfU2;
+    }
+
+    public List<String> getTitlesOfBobDylan() {
+        return titlesOfBobDylan;
+    }
+
+    public Song getRandomSongOfCD() {
+        return randomSongOfCD;
     }
 }
--- src/main/java/_01_core_spring/_03_advanced_wiring/_05_runtime_value_injection/_02_wiring_with_the_spring_expression_language/_07_evaluating_collections/Launcher.java
+++ src/main/java/_01_core_spring/_03_advanced_wiring/_05_runtime_value_injection/_02_wiring_with_the_spring_expression_language/_07_evaluating_collections/Launcher.java
@@ -7,9 +7,33 @@ public class Launcher {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("listing_in_01_03_05_02_07/springConf.xml");
         FilteredSongs filteredSongs = context.getBean(FilteredSongs.class);
+        System.out.println("Songs of U2");
         for(Song s : filteredSongs.getSongsOfU2()) {
             System.out.println(s.getAuthor() + " " + s.getTitle());
         }
+        System.out.print("\n");
+        System.out.println("Songs of Bob Dylan");
+        for(Song s : filteredSongs.getSongsOfBobDylan()) {
+            System.out.println(s.getAuthor() + " " + s.getTitle());
+        }
+        System.out.println("");
+        System.out.println(filteredSongs.getFirstSongOfU2().getTitle());
+        System.out.println("");
+        System.out.println(filteredSongs.getFirstSongOfBobDylan().getTitle());
+        System.out.println("");
+        System.out.println(filteredSongs.getLastSongOfU2().getTitle());
+        System.out.println("");
+        System.out.println(filteredSongs.getLastSongOfBobDylan().getTitle());
+        System.out.println("");
+        for (String s : filteredSongs.getTitlesOfU2()){
+            System.out.println(s);
+        }
+        System.out.println("");
+        for (String s : filteredSongs.getTitlesOfBobDylan()){
+            System.out.println(s);
+        }
+        System.out.println("");
+        System.out.println(filteredSongs.getRandomSongOfCD().getTitle());
         context.close();
     }
 }
