9108c234340ae6caf29d51bf1e4adf5d25b4973d
fguenzi <fguenzi@egencia.com>
2016-10-26 09:53:57 +0200

    trying to understand when I fall into : Failed to register servlet with name 'dispatcher'.Check if there is another servlet registered under the same name.

--- src/main/java/_02_spring_on_the_web/_05_building_spring_web_applications/_02_writing_a_simple_controller/MyAppEntryConfig.java
+++ src/main/java/_02_spring_on_the_web/_05_building_spring_web_applications/_02_writing_a_simple_controller/MyAppEntryConfig.java
@@ -2,19 +2,16 @@ package _02_spring_on_the_web._05_building_spring_web_applications._02_writing_a
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class MyAppEntryConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class MyAppEntryConfig /*extends AbstractAnnotationConfigDispatcherServletInitializer*/ {
 
-    @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[] { RootConfig.class };
     }
 
-    @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[] { ServletConfig.class };
     }
 
-    @Override
     protected String[] getServletMappings() {
         return new String[] { "/" };
     }
--- src/main/java/_02_spring_on_the_web/_05_building_spring_web_applications/_02_writing_a_simple_controller/_02_defining_class_level_request_handling/ConfigDispatcher.java
+++ src/main/java/_02_spring_on_the_web/_05_building_spring_web_applications/_02_writing_a_simple_controller/_02_defining_class_level_request_handling/ConfigDispatcher.java
@@ -2,16 +2,17 @@ package _02_spring_on_the_web._05_building_spring_web_applications._02_writing_a
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class ConfigDispatcher extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class ConfigDispatcher /*extends AbstractAnnotationConfigDispatcherServletInitializer*/ {
+
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{RootConfig.class};
+        return new Class<?>[] { RootConfig.class };
     }
 
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[]{ServletConfig.class};
+        return new Class<?>[] { ServletConfig.class };
     }
 
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[] { "/" };
     }
 }
